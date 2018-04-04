f7346e2139ba206ff059f58eb8039c9aaf84596f
zyxue <alfred532008@gmail.com>
2018-04-03 21:59:13 -0700

    formatted InitNet in word2vec.c for better readability

--- src/word2vec.c
+++ src/word2vec.c
@@ -338,24 +338,55 @@ void ReadVocab() {
 void InitNet() {
   long long a, b;
   unsigned long long next_random = 1;
-  a = posix_memalign((void **)&syn0, 128, (long long)vocab_size * layer1_size * sizeof(real));
-  if (syn0 == NULL) {printf("Memory allocation failed\n"); exit(1);}
-  if (hs) {
-    a = posix_memalign((void **)&syn1, 128, (long long)vocab_size * layer1_size * sizeof(real));
-    if (syn1 == NULL) {printf("Memory allocation failed\n"); exit(1);}
-    for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++)
-     syn1[a * layer1_size + b] = 0;
+  a = posix_memalign(
+      (void **)&syn0,
+      128,
+      (long long)vocab_size * layer1_size * sizeof(real)
+  );
+
+  if (syn0 == NULL) {
+    printf("Memory allocation failed\n");
+    exit(1);
   }
+
+  if (hs) { // hierarchical softmax
+    a = posix_memalign(
+        (void **)&syn1,
+        128,
+        (long long)vocab_size * layer1_size * sizeof(real)
+    );
+    if (syn1 == NULL) {
+        printf("Memory allocation failed\n"); exit(1);
+    }
+    for (a = 0; a < vocab_size; a++) {
+        for (b = 0; b < layer1_size; b++) {
+            syn1[a * layer1_size + b] = 0;
+        }
+    }
+  }
+
   if (negative>0) {
-    a = posix_memalign((void **)&syn1neg, 128, (long long)vocab_size * layer1_size * sizeof(real));
-    if (syn1neg == NULL) {printf("Memory allocation failed\n"); exit(1);}
-    for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++)
-     syn1neg[a * layer1_size + b] = 0;
+    a = posix_memalign(
+        (void **)&syn1neg,
+        128,
+        (long long)vocab_size * layer1_size * sizeof(real)
+    );
+    if (syn1neg == NULL) {
+        printf("Memory allocation failed\n"); exit(1);
+    }
+    for (a = 0; a < vocab_size; a++) {
+        for (b = 0; b < layer1_size; b++) {
+            syn1neg[a * layer1_size + b] = 0;
+        }
+    }
   }
-  for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++) {
-    next_random = next_random * (unsigned long long)25214903917 + 11;
-    syn0[a * layer1_size + b] = (((next_random & 0xFFFF) / (real)65536) - 0.5) / layer1_size;
+  for (a = 0; a < vocab_size; a++) {
+      for (b = 0; b < layer1_size; b++) {
+        next_random = next_random * (unsigned long long)25214903917 + 11;
+        syn0[a * layer1_size + b] = (((next_random & 0xFFFF) / (real)65536) - 0.5) / layer1_size;
+      }
   }
+
   CreateBinaryTree();
 }
 
